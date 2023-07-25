71c011014d783e99ea8d5d96b56dcca7b27b2f29
NZT48 <nikolaztodorovic26@gmail.com>
2023-07-25 15:23:32 +0200

    Change all blockchain names to OTP to fix get bid suggestion issue

--- dkg/constants.py
+++ dkg/constants.py
@@ -4,15 +4,15 @@ PRIVATE_ASSERTION_PREDICATE = (
 
 BLOCKCHAINS = {
         2043: {
-            "name": "otp_mainnet",
+            "name": "otp",
             "hubAddress": "0x5fA7916c48Fe6D5F1738d12Ad234b78c90B4cAdA"
         },
         2160: {
-            "name": "otp_devnet",
+            "name": "otp",
             "hubAddress": "0x833048F6e6BEa78E0AAdedeCd2Dc2231dda443FB"
         },
         20430: {
-            "name": "otp_testnet",
+            "name": "otp",
             "hubAddress": "0xBbfF7Ea6b2Addc1f38A0798329e12C08f03750A6"
         },
         31337:  {
