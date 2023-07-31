16c9d0dcfe0713f99e015c695181239a560f4d6c
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2023-07-31 22:45:16 +0100

    Changed license to Apache-2.0

--- dkg/providers/blockchain.py
+++ dkg/providers/blockchain.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import json
 from collections import namedtuple
 from pathlib import Path
@@ -13,6 +30,7 @@ from web3.logs import DISCARD
 from web3.middleware import construct_sign_and_send_raw_middleware
 from web3.types import ABI, ABIFunction, TxReceipt
 from dkg.constants import BLOCKCHAINS
+
 
 class BlockchainProvider:
     CONTRACTS_METADATA_DIR = Path(__file__).parents[1] / "data/interfaces"
